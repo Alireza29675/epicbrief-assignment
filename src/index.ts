7414ce0bb1e195d814556296f86f898b1245aee0
Alireza <contact@alireza.cc>
2023-01-31 23:49:04 +0100

    feat(meetings): meetings are being auto-summarised

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,50 @@
 // Loading the environment variables before anything else
 import './loadEnv';
+
 import dealsIntegration from './integrations/hubspot/deals';
 import meetingsIntegration from './integrations/hubspot/meetings';
+import meetings from './models/meetings';
 
-dealsIntegration.sync();
-meetingsIntegration.sync();
+// This is the main function that will be called by the cron job
+const main = async () => {
+  // Syncing with Hubspot at the beginning
+  await meetingsIntegration.sync();
+  await dealsIntegration.sync();
+
+  // Summarizing new meetings every 10 seconds
+  setInterval(async () => {
+    // Adding summaries to all meetings
+    await addSummaryToAllMeetingsWithoutSummary();
+
+    // Syncing the meetings with Hubspot
+    await meetingsIntegration.sync();
+  }, 10_000);
+};
+
+main();
+
+// ------------------------------------------------------------
+// 👇 DUMMY CODE BELOW - JUST FOR PRESENTING THE CONCEPT 👇
+// ------------------------------------------------------------
+
+// This is a dummy summarizer that just takes the first 10 words of the body
+const dummySummarizer = (body: string) => {
+  const words = body.split(' ');
+  const summaryCount = Math.min(10, Math.floor(words.length / 2));
+  return body.split(' ').slice(0, summaryCount).join(' ');
+};
+
+const addSummaryToAllMeetingsWithoutSummary = async () => {
+  for (const meeting of meetings.data) {
+    // If the meeting has no summary, we need to generate one
+    if (!meeting.summary.trim()) {
+      // This is the dummy summarizer
+      const summary = dummySummarizer(meeting.body);
+
+      // Update the meeting with the new summary
+      await meetings.update(meeting.id, {
+        summary,
+      });
+    }
+  }
+};
--- src/integrations/hubspot/meetings.ts
+++ src/integrations/hubspot/meetings.ts
@@ -1,6 +1,7 @@
 import meetings from '@/models/meetings';
 import hubspot from '@/services/hubspot';
 import createIntegrations from '../utils/createIntegration';
+import { convert as htmlToText } from 'html-to-text';
 
 const api = hubspot.crm.objects.meetings.basicApi;
 
@@ -11,6 +12,7 @@ const meetingsIntegration = createIntegrations({
     fetch: async () => {
       // Fetch all meetings from Hubspot
       // What are those two undefineds? Well, the hubspot library's api is a bit weird:)
+      // I could also use the api itself and call it using axios
       const { results: meetings } = await api.getPage(undefined, undefined, [
         'hs_internal_meeting_notes',
         'hs_meeting_body',
@@ -19,8 +21,9 @@ const meetingsIntegration = createIntegrations({
       return meetings.map(({ id, properties, updatedAt }) => ({
         id,
         _updatedAt: updatedAt.getTime(),
-        body: properties['hs_meeting_body'],
-        summary: properties['hs_internal_meeting_notes'],
+        // We need to convert the html to text because the hubspot api send the data as html
+        body: htmlToText(properties['hs_meeting_body']),
+        summary: htmlToText(properties['hs_internal_meeting_notes']),
       }));
     },
     update: async (id, data) => {

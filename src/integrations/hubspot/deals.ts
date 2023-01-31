bcbbbb56b8333604f02c851cfb3073b3e48d6715
Alireza <contact@alireza.cc>
2023-01-31 20:44:17 +0100

    docs(deals): some comments updated

--- src/integrations/hubspot/deals.ts
+++ src/integrations/hubspot/deals.ts
@@ -1,19 +1,17 @@
 import deals from '@/models/deals';
 import hubspot from '@/services/hubspot';
-import createIntegrations from '../utils/createIntegration';
+import createIntegration from '../utils/createIntegration';
 
 const api = hubspot.crm.deals.basicApi;
 
-const dealsIntegration = createIntegrations({
+const dealsIntegration = createIntegration({
   model: deals,
   service: {
     name: 'hubspot/deals',
 
-    // Fetch all deals from hubspot
+    // Fetch all deals from Hubspot
     fetch: async () => {
-      const deals = (await api.getPage()).results;
-
-      // Transform the data to match the model
+      const { results: deals } = await api.getPage();
       return deals.map(({ id, properties, updatedAt }) => ({
         id,
         _updatedAt: updatedAt.getTime(),
@@ -23,31 +21,28 @@ const dealsIntegration = createIntegrations({
       }));
     },
 
-    // Defining the method to create a deal if needed
-    create: async (deal) => {
-      const createdDeal = await api.create({
+    // Creates a deal in Hubspot
+    create: async ({ name, amount, stage }) => {
+      const { id: createdDealId } = await api.create({
         properties: {
-          dealname: deal.name,
-          amount: `${deal.amount}`,
-          dealstage: deal.stage,
+          dealname: name,
+          amount: `${amount}`,
+          dealstage: stage,
         },
       });
-
-      return createdDeal.id;
+      return createdDealId;
     },
 
-    // Defining the method to delete a deal if needed
-    delete: async (id) => {
-      await api.archive(id);
-    },
+    // Deletes a deal from Hubspot
+    delete: async (id) => api.archive(id),
 
-    // Defining the method to update a deal if needed
-    update: async (id, deal) => {
-      await api.update(id, {
+    // Updates a deal in Hubspot
+    update: async (id, { name, amount, stage }) => {
+      api.update(id, {
         properties: {
-          dealname: deal.name,
-          amount: `${deal.amount}`,
-          dealstage: deal.stage,
+          dealname: name,
+          amount: `${amount}`,
+          dealstage: stage,
         },
       });
     },
